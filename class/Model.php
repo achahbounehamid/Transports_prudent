e86f133cd9ed91b454126cdd21fe6faa1f29c3cb
achahbounehamid <120572132+achahbounehamid@users.noreply.github.com>
2023-11-02 00:08:28 +0100

    page Commerce chart.js

--- class/Model.php
+++ class/Model.php
@@ -1,19 +1,34 @@
 <?php
 abstract class Model {
-    private static $db;
+    // private static $db;
+
+    // private static function setDb(){
+    //     try {
+    //         self::$db = new PDO('mysql:host=localhost;dbname=transpotrs_prudent;charset=utf8', 'root');
+    //     } catch(PDOException $e){
+    //         echo "Erreur :" . $e->getMessage();
+    //     }
+    // }
+
+    // protected function getDb(){
+    //     if(self::$db == null){
+    //         self::setDb();
+    //     }
+    //     return self::$db;
+    // }
 
     private static function setDb(){
         try {
-            self::$db = new PDO('mysql:host=localhost;dbname=transpotrs_prudent;charset=utf8', 'root');
+            $options = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Activer le mode d'erreur exception
+                PDO::ATTR_EMULATE_PREPARES => false, // Désactiver l'émulation des requêtes préparées
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Récupérer les résultats sous forme de tableau associatif par défaut
+            );
+    
+            self::$db = new PDO('mysql:host=localhost;dbname=transpotrs_prudent;charset=utf8', 'root', '', $options);
         } catch(PDOException $e){
-            echo "Erreur :" . $e->getMessage();
+            echo "Erreur : " . $e->getMessage();
         }
-    }
-
-    protected function getDb(){
-        if(self::$db == null){
-            self::setDb();
-        }
-        return self::$db;
+       
     }
 }
