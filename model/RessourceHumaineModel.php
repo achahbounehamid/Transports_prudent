5ef6e8ca55a2cc05bfa8f8b25f530249e7a73c17
achahbounehamid <120572132+achahbounehamid@users.noreply.github.com>
2023-11-22 16:52:45 +0100

    2eme graphique Rh

--- model/RessourceHumaineModel.php
+++ model/RessourceHumaineModel.php
@@ -24,23 +24,30 @@ class RessourceHumaineModel extends Model
         // Récupérez les résultats sous forme de tableau associatif
         $resultSatisfaction = $stmtSatisfaction->fetchAll(PDO::FETCH_ASSOC);
 
-        // // Préparez la requête pour la répartition homme/femme
-        // $queryGenderDistribution = "SELECT titreDemploi AS Metier, 
-        //    SUM(CASE WHEN sexe = 'Homme' THEN 1 ELSE 0 END) AS Hommes,
-        //    SUM(CASE WHEN sexe = 'Femme' THEN 1 ELSE 0 END) AS Femmes
-        // FROM resource_humaine GROUP BY titreDemploi";
-        // $stmtGenderDistribution = $this->getDb()->prepare($queryGenderDistribution);
+        // Préparez la requête pour la répartition homme/femme
+        $queryGenderDistribution = "SELECT titreDemploi AS Metier, 
+           SUM(CASE WHEN sexe = 'Homme' THEN 1 ELSE 0 END) AS Hommes,
+           SUM(CASE WHEN sexe = 'Femme' THEN 1 ELSE 0 END) AS Femmes
+        FROM resource_humaine GROUP BY titreDemploi";
+        $stmtGenderDistribution = $this->getDb()->prepare($queryGenderDistribution);
 
-        // // Exécutez la requête pour la répartition homme/femme
-        // $stmtGenderDistribution->execute();
+        // Exécutez la requête pour la répartition homme/femme
+        $stmtGenderDistribution->execute();
 
-        // // Récupérez les résultats sous forme de tableau associatif
-        // $resultGenderDistribution = $stmtGenderDistribution->fetchAll(PDO::FETCH_ASSOC);
+        // Récupérez les résultats sous forme de tableau associatif
+        $resultGenderDistribution = $stmtGenderDistribution->fetchAll(PDO::FETCH_ASSOC);
+
+        $queryFichierEmploiyes = "SELECT Nom, Prénom, Sexe, Departement, TitreDemploi, DateDEmbauche FROM resource_humaine";
+        $stmtFichierEmploiyes = $this->getDb()->prepare($queryFichierEmploiyes);
+        $stmtFichierEmploiyes->execute();
+
+        $resultFichierEmploiyes = $stmtFichierEmploiyes->fetchAll(PDO::FETCH_ASSOC);
 
         return [
             'resultAbsenteeism' => $resultAbsenteeism,
             'resultSatisfaction' => $resultSatisfaction,
-            // 'resultGenderDistribution' => $resultGenderDistribution,
+            'resultrepartitionHommesFemmes' => $resultGenderDistribution,
+            'resultFichierEmploiyes'=> $resultFichierEmploiyes,
         ];
     }
 }
