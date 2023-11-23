ac97accfbeffdcc5540406c0522ff2f09f2c013e
elisa-1987 <elisaravinet@icloud.com>
2023-11-23 10:01:15 +0100

    Revert "2eme graphique Rh"
    This reverts commit 5ef6e8ca55a2cc05bfa8f8b25f530249e7a73c17.

--- asset/js/ressourceHumaine.js
+++ asset/js/ressourceHumaine.js
@@ -36,6 +36,7 @@
 let satisfactionChart= dataSatisfaction.map(item => {
     return { x: item.Metier, y: parseFloat(item.SatisfactionMoyenne) }; // Convertissez la chaîne en nombre
 });
+console.log(satisfactionChart);
 // Créez le graphique d'histogramme
 let chart = new ej.charts.Chart({
     // Configuration du graphique d'histogramme
@@ -56,47 +57,5 @@ let chart = new ej.charts.Chart({
     ],
 },'#satisfactionChartContainer');
 
-//graphique 
 
-
-// let piechart = new ej.charts.AccumulationChart({
-//     series: [{
-//         dataSource: piechart,
-       
-//         dataLabel: {
-//             visible: true,
-//             position: 'Outside',
-//             name: 'x'
-//         },
-//         radius: 'r',
-//         xName: 'Metier', // Assurez-vous que cela correspond à votre structure de données
-//         yName: 'Hommes', // Assurez-vous que cela correspond à votre structure de données
-//         innerRadius: '20%'
-//     }],
-//     enableSmartLabels: true,
-//     legendSettings: {
-//         visible: true,
-//     },
-//     enableAnimation: true,
-//     title: 'Répartition Hommes/Femmes par Métier'
-    
-// },'#absenteeismChartContainer');
-
-//graphique Pyramide
-// Données de la série pour le graphique Pyramide
-let pyramidChartData = dataFichierEmploiyes.map(item => {
-    return { x: item.x, y: item.y, text: item.text }; // Assurez-vous que cela correspond à votre structure de données
-});
-
-// Configuration du graphique Pyramide
-let pyramidChart = new ej.charts.AccumulationChart({
-    width: '600px',
-    series: [{
-        type: 'Pyramid',
-        dataSource: pyramidChartData,
-        xName: 'Nom',
-         yName: 'TitreDemploi',
-        dataLabel: { name: 'text', visible: true, position: 'Inside' },
-    }],
-    title: '',
-}, '#FichierEmploiyes');
+  
