be6ed40a6ed4c1a295eeb57a166476d57215abd8
elisa-1987 <elisaravinet@icloud.com>
2023-12-05 09:00:46 +0100

    graphique

--- asset/js/ressourceHumaine.js
+++ asset/js/ressourceHumaine.js
@@ -1,36 +1,63 @@
-document.addEventListener('DOMContentLoaded', function () {
-    
 
-// Vérifiez si data est un tableau
-if (Array.isArray(data)) {
-    // Créez un tableau de données dans le format approprié pour Syncfusion
-    let dataSource = [];
-    data.forEach(item => {
-        dataSource.push({
-            type: item.type,
-            Metier: item.Metier,
-        });
-    });
-    console.log(data);
-    let genderDistributionChart = new ej.charts.Chart({
-        series: [
-            {
-                type: 'Doughnut',
-                dataSource: dataSource,
-                xName: 'Metier',
-                yName: 'Hommes',
-                name: 'Hommes'
-            },
-            {
-                type: 'Doughnut',
-                dataSource: dataSource,
-                xName: 'Metier',
-                yName: 'Femmes',
-                name: 'Femmes'
-            }
-        ],
-        title: 'Répartition Homme-Femme par Métier'
-    });
-}
-});
+let tauxAbsence = new ej.charts.Chart({
+    primaryXAxis: {
+        valueType: 'Category',
+        interval: 1, // Afficher chaque mois
+        labelIntersectAction: 'Rotate45', // Rotation des étiquettes pour une meilleure visibilité
+    },
+    primaryYAxis: {
+        // Ajustez la valeur maximale en conséquence
+       
+        labelFormat: '{value}%', // Format des étiquettes de l'axe des y en pourcentage
+    },
+    series: [{
+        dataSource: dataTauxAbsence,
+        xName: 'libelle',
+        yName: 'taux_moyen_arrondi',
+        name: '2022',
+        type: 'Column'
+    },
+    {
+        dataSource: dataTauxAbsence,
+        xName: 'libelle',
+        yName: 'taux_moyen_arrondi',
+        name: '2023',
+        columnWidth: 0.8,
+        columnSpacing: 1.5,
+        type: 'Column',
+    }],
+    title: 'Taux d\absentéisme',
 
+}, '.chart-placeholder');
+
+let masseSalaire = new ej.charts.Chart({
+    primaryXAxis: {
+        valueType: 'Category',
+        interval: 1, // Afficher chaque mois
+        labelIntersectAction: 'Rotate45', // Rotation des étiquettes pour une meilleure visibilité
+    },
+    primaryYAxis: {
+        // Ajustez la valeur maximale en conséquence
+        // interval: 5, 
+        labelFormat: '{value}', // Format des étiquettes de l'axe des y en pourcentage
+    },
+    series: [{
+        dataSource: dataMasseSalaire,
+        xName: 'libelle',
+        yName: 'moyenne_mensuelle',
+        name: '2022',
+        type: 'Column',
+        fill: '#24ECE3',
+    },
+    {
+        dataSource: dataMasseSalaire,
+        xName: 'libelle',
+        yName: 'moyenne_mensuelle',
+        name: '2023',
+        columnWidth: 0.8,
+        columnSpacing: 1.5,
+        type: 'Column',
+        fill: '#2196F3',
+    }],
+    title: 'Evolution de la masse salariale'
+}, '#absenteeismChartContainer');
--- model/RessourceHumaineModel.php
+++ model/RessourceHumaineModel.php
@@ -4,43 +4,33 @@ class RessourceHumaineModel extends Model
 {
     public function getRessourceHumaine()
     {
-        // Préparez la requête pour le taux d'absentéisme
-        $queryAbsenteeism = "SELECT titreDemploi AS Metier, AVG(tauxDAbsenteisme) AS TauxAbsenteismeMoyen FROM resource_humaine GROUP BY titreDemploi";
-        $stmtAbsenteeism = $this->getDb()->prepare($queryAbsenteeism);
 
-        // Exécutez la requête pour le taux d'absentéisme
-        $stmtAbsenteeism->execute();
+        // Préparez la requête pour le taux d'absenteisme
+        $reqTauxAbsence = $this->getDb()->prepare('SELECT libelle, annee, ROUND((taux)*100, 0) AS taux_moyen_arrondi
+        FROM taux_absence
+        WHERE annee IN (2022, 2023)
+        GROUP BY libelle, annee
+        ');
+        $reqTauxAbsence->execute();
 
-        // Récupérez les résultats sous forme de tableau associatif
-        $resultAbsenteeism = $stmtAbsenteeism->fetchAll(PDO::FETCH_ASSOC);
+        // Récupérer les résultats sous forme de tableau associatif
+        $resultMassSalariale = $reqTauxAbsence->fetchAll(PDO::FETCH_ASSOC);
+        // return ['resultTauxAbsence'=>$resultMassSalariale];
 
-        // Préparez la requête pour la satisfaction
-        $querySatisfaction = "SELECT titreDemploi AS Metier, AVG(satisfaction) AS SatisfactionMoyenne FROM resource_humaine GROUP BY titreDemploi";
-        $stmtSatisfaction = $this->getDb()->prepare($querySatisfaction);
+        // Préparez la requête pour Evolution de la masse salaire
+        $reqMasseSalaire = $this->getDb()->prepare('SELECT annee, libelle, ROUND(AVG(montant), 0) AS moyenne_mensuelle
+        FROM masse_salariale
+        GROUP BY annee, mois;
+        ');
+        $reqMasseSalaire->execute();
 
-        // Exécutez la requête pour la satisfaction
-        $stmtSatisfaction->execute();
+        // Récupérer les résultats sous forme de tableau associatif
+        $resultMasseSalaire = $reqMasseSalaire->fetchAll(PDO::FETCH_ASSOC);
 
-        // Récupérez les résultats sous forme de tableau associatif
-        $resultSatisfaction = $stmtSatisfaction->fetchAll(PDO::FETCH_ASSOC);
+        return ['resultTauxAbsence'=>$resultMassSalariale,'resultMasseSalaire'=>$resultMasseSalaire,
+    ];
 
-        // Préparez la requête pour la répartition homme/femme
-        $queryGenderDistribution = "SELECT titreDemploi AS Metier, 
-           SUM(CASE WHEN sexe = 'Homme' THEN 1 ELSE 0 END) AS Hommes,
-           SUM(CASE WHEN sexe = 'Femme' THEN 1 ELSE 0 END) AS Femmes
-        FROM resource_humaine GROUP BY titreDemploi";
-        $stmtGenderDistribution = $this->getDb()->prepare($queryGenderDistribution);
 
-        // Exécutez la requête pour la répartition homme/femme
-        $stmtGenderDistribution->execute();
-
-        // Récupérez les résultats sous forme de tableau associatif
-        $resultGenderDistribution = $stmtGenderDistribution->fetchAll(PDO::FETCH_ASSOC);
-
-        return [
-            'resultAbsenteeism' => $resultAbsenteeism,
-            'resultSatisfaction' => $resultSatisfaction,
-            'resultGenderDistribution' => $resultGenderDistribution,
-        ];
     }
+
 }
